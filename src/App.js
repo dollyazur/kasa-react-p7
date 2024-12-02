0e727dd9f8afc258b1b6bf791ad28b3cae51f04e
dolly.azur <azurgrisby@hotmail.fr>
2024-12-02 11:40:53 +0100

    essai npm start et yarn start, creation dossier components dans src et fichier banner dans components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Da web inside</p> {/*nom de mon site*/}
+        <p>Da web inside</p>
         <a
           className="App-link"
           href="https://reactjs.org"
